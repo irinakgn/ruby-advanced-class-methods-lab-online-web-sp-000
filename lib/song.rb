8f4d8a734b74e6eb6a3a63e6ed3e213ba85d695d
Irina Kagan <irinagamb@gmail.com>
2019-05-06 00:22:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,15 +21,11 @@ class Song
      # and the song being saved into the @@all class variable.
    end
 
-   def self.new_by_name(string_name_of_the_song)
-     # takes in the string name of a song and returns a
-     # song instance with that name set as its name property.
+   def self.new_by_name(name)
+     song = self.new
+     song.name = name
+     song
 
-     # should return an instance of Song and not a simple string or anything else.
-     s = self.new
-     s.name = string_name_of_the_song
-     s
-     # there's nothing in the instructions about saving to the @@all class variable
    end
 
    def self.find_by_name(name)
